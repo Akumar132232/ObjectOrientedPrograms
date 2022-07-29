9907fcce3709432a5a841858de4f066578721e4c
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-29 23:15:01 +0530

    Create MaximumProblem.java

---
+++ MaximumProblem.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaximumProblem
+{
+
+    public static void main(String[] args)
+    {
+
+        Operation operation = new Operation();
+
+        operation.getInputForInteger();
+        int integerMax =operation.testMaximum(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
+        System.out.println(integerMax+" is maximum amongst integer numbers");
+
+        operation.getInputForFloat();
+        Float floatMax =operation.testMaximum(operation.number1,operation.number2,operation.number3);
+        System.out.println(floatMax+" is maximum amongst float numbers");
+
+        operation.getInputForString();
+        String stringMax =operation.testMaximum(operation.string1,operation.string2,operation.string3);
+        System.out.println(stringMax+" is maximum alphabetically amongst strings");
+
+    }
+
+}
